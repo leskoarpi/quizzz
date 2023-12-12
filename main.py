99e8ed1ad539ac7b7ee8cdc62a417071ad69f025
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2023-12-12 17:04:48 +0000

    it kinda works

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import streamlit as st 
 import streamlit_authenticator as stauth 
+
+st.title("Welcome, Human!")
+
+data = st.text_input("data")
+
+if 'lista' not in st.session_state:
+    st.session_state.lista = []
+
+if st.button('hozzaad'):
+    st.session_state.lista.append(data)
+
+if st.session_state.lista:
+    for i in range(len(st.session_state.lista)):
+        st.write(st.session_state.lista[i])
